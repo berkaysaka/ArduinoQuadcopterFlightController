f408c9a78e8ff742d10e3d248c5fc2af98a5e379
berkaysaka <berkaysaka@yahoo.com>
2020-01-09 18:45:36 +0000

    add yaw tests

--- quadcopter/tests.ino
+++ quadcopter/tests.ino
@@ -8,7 +8,8 @@ void reset_test_parameters(){
   pitchAngle = 0;
   yawAngle = 0;
   roll_pid_i = 0; roll_last_error = 0; pitch_pid_i = 0; pitch_last_error = 0; yaw_pid_i = 0; yaw_last_error = 0;
-  last_time = millis()-1;
+  last_time = millis() - 1;
+  frontLeftMotorPower = 0; rearRightMotorPower = 0; frontRightMotorPower = 0; rearLeftMotorPower = 0;
 }
 void run_tests(){
   Serial.println("----------------RUNNING TESTS--------------");
@@ -24,6 +25,8 @@ void test_calculate_motor_powers(){
   test_calculate_motor_powers_should_run_front_motors_faster_to_go_backwards();
   test_calculate_motor_powers_should_run_right_motors_faster_to_go_left();
   test_calculate_motor_powers_should_run_left_motors_faster_to_go_right();
+  test_calculate_motor_powers_should_run_right_turning_motors_faster_to_turn_left();
+  test_calculate_motor_powers_should_run_left_turning_motors_faster_to_turn_right();
 }
 
 void test_calculate_motor_powers_should_set_turn_off_motors_if_receiver_is_unplugged(){
@@ -59,7 +62,6 @@ void test_calculate_motor_powers_should_run_rear_motors_faster_to_go_forward(){
   
   bool passed = (frontLeftMotorPower == frontRightMotorPower) && (rearLeftMotorPower == rearRightMotorPower) && (frontLeftMotorPower < rearLeftMotorPower);
   Serial.println(String(passed) + " => test_calculate_motor_powers_should_run_rear_motors_faster_to_go_forward");
-  printCurrentMotorPowers();
 }
 
 void test_calculate_motor_powers_should_run_front_motors_faster_to_go_backwards(){
@@ -70,7 +72,6 @@ void test_calculate_motor_powers_should_run_front_motors_faster_to_go_backwards(
   
   bool passed = (frontLeftMotorPower == frontRightMotorPower) && (rearLeftMotorPower == rearRightMotorPower) && (frontLeftMotorPower > rearLeftMotorPower);
   Serial.println(String(passed) + " => test_calculate_motor_powers_should_run_front_motors_faster_to_go_backwards");
-  printCurrentMotorPowers();
 }
 
 void test_calculate_motor_powers_should_run_right_motors_faster_to_go_left(){
@@ -92,6 +93,28 @@ void test_calculate_motor_powers_should_run_left_motors_faster_to_go_right(){
   
   bool passed = (frontLeftMotorPower == rearLeftMotorPower) && (frontRightMotorPower == rearRightMotorPower) && (rearRightMotorPower < rearLeftMotorPower);
   Serial.println(String(passed) + " => test_calculate_motor_powers_should_run_left_motors_faster_to_go_right");
+  printCurrentMotorPowers();
+}
+
+void test_calculate_motor_powers_should_run_right_turning_motors_faster_to_turn_left(){
+  reset_test_parameters();
+  throttle = 20;
+  desired_yaw_angle = -5;
+  calculateMotorPowers();
+  
+  bool passed = (frontLeftMotorPower == rearRightMotorPower) && (frontRightMotorPower == rearLeftMotorPower) && (rearRightMotorPower > rearLeftMotorPower);
+  Serial.println(String(passed) + " => test_calculate_motor_powers_should_run_right_turning_motors_faster_to_turn_left");
+  printCurrentMotorPowers();
+}
+
+void test_calculate_motor_powers_should_run_left_turning_motors_faster_to_turn_right(){
+  reset_test_parameters();
+  throttle = 20;
+  desired_yaw_angle = 5;
+  calculateMotorPowers();
+  
+  bool passed = (frontLeftMotorPower == rearRightMotorPower) && (frontRightMotorPower == rearLeftMotorPower) && (rearRightMotorPower < rearLeftMotorPower);
+  Serial.println(String(passed) + " => test_calculate_motor_powers_should_run_left_turning_motors_faster_to_turn_right");
   printCurrentMotorPowers();
 }
 
