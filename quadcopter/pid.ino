8043083f4b6143747a82e6b30521266870f17adc
berkaysaka <berkaysaka@yahoo.com>
2020-01-09 21:58:54 +0000

    fix pid derivative logic

--- quadcopter/pid.ino
+++ quadcopter/pid.ino
@@ -2,7 +2,7 @@ double getControlSignal(double error, double kp, double ki, double kd, double& p
   double pid_p = kp * error;
   pid_i += ki * error;
   pid_i = constrain(pid_i, (-1 * integral_limit), integral_limit);
-  double pid_d = kd * ((error - last_error) / delta_time / 1000);
+  double pid_d = kd * 1000 * ((error - last_error) / delta_time);
   double control_signal = pid_p + pid_d + pid_i;
   last_error = error;
   return control_signal;
