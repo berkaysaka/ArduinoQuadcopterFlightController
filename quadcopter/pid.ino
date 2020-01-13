7fec27e032bf3370804e172f30204776b433e38a
berkaysaka <berkaysaka@yahoo.com>
2020-01-13 23:04:06 +0000

    prevent derivative kicks

--- quadcopter/pid.ino
+++ quadcopter/pid.ino
@@ -2,13 +2,19 @@ double getControlSignal(double error, double kp, double ki, double kd, double& p
   pid_i += error * delta_time;
   pid_i = constrain(pid_i, -integral_limit, integral_limit);
   if ((error<=0 && last_error>0) || (error>=0 && last_error<0)){
-    pid_i = 0; // fix integral wind-up by resettin integral to prevent unnecessary overshoot)
+    pid_i = 0; // fix integral wind-up isuue by resetting integral to zero in order to prevent unnecessary overshoots)
   }
   if (abs(error) > integral_activation_threshold){
-    pid_i = 0; // fix kicking off integral early
+    pid_i = 0; // don't start counting integral early
   }
+  
   double pid_p = error;
+  
   double pid_d = (error - last_error) / delta_time;
+  if ((error > 0 && (error - last_error) > 0) || (error < 0 && (error - last_error) < 0)){
+    pid_d = 0; // prevent derivative kicks
+  }
+  
   double control_signal = (kp*pid_p) + (kd*pid_d) + (ki*pid_i);
   last_error = error;
   return control_signal;
