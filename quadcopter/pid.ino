636fb95705e137e0107e7a270b60324c643da4fc
berkaysaka <berkaysaka@yahoo.com>
2020-01-13 22:36:15 +0000

    fix integral windup

--- quadcopter/pid.ino
+++ quadcopter/pid.ino
@@ -1,6 +1,9 @@
 double getControlSignal(double error, double kp, double ki, double kd, double& pid_i, double& last_error, double integral_limit) {
   pid_i += error * delta_time;
   pid_i = constrain(pid_i, -integral_limit, integral_limit);
+  if((error<=0 && last_error>0) || (error>=0 && last_error<0)){
+    pid_i = 0; // fix integral wind-up by resettin integral to prevent unnecessary overshoot)
+  }
   double pid_p = error;
   double pid_d = (error - last_error) / delta_time;
   double control_signal = (kp*pid_p) + (kd*pid_d) + (ki*pid_i);
