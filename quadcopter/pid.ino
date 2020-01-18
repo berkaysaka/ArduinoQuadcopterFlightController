2183ac8ca0ce3ec553ec1ec13352766d0c814600
berkaysaka <berkaysaka@yahoo.com>
2020-01-18 23:32:07 +0000

    fix integral bug

--- quadcopter/pid.ino
+++ quadcopter/pid.ino
@@ -1,8 +1,7 @@
 double getControlSignal(double error, double kp, double ki, double kd, double& pid_i, double& last_error, double integral_limit, double integral_activation_threshold) {
   pid_i += error * delta_time;
-  pid_i = constrain(pid_i, -integral_limit, integral_limit);
   if ((error<0. && last_error>0.) || (error>0. && last_error<0.)){
-    //pid_i = 0; // fix integral wind-up issue by resetting integral to zero in order to prevent unnecessary overshoots)
+    pid_i = 0; // fix integral wind-up issue by resetting integral to zero in order to prevent unnecessary overshoots)
   }
   if (abs(error) > integral_activation_threshold){
     pid_i = 0; // don't start counting integral early
@@ -15,7 +14,7 @@ double getControlSignal(double error, double kp, double ki, double kd, double& p
     pid_d = 0; // prevent derivative kicks
   }
   
-  double control_signal = (kp*pid_p) + (ki*pid_i) + (kd*pid_d);
+  double control_signal = (kp*pid_p) + constrain(ki*pid_i, -integral_limit, integral_limit) + (kd*pid_d);
   last_error = error;
   return control_signal;
 }
