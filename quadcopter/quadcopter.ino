d7ce459756a2378502adb00fc37744b63c44c0a1
Berkay Saka <berkaysaka@yahoo.com>
2020-02-10 19:44:41 +0000

    change pid values

--- quadcopter/quadcopter.ino
+++ quadcopter/quadcopter.ino
@@ -14,16 +14,16 @@
 #define THROTTLE_START_POINT 15
 #define THROTTLE_LIMIT_POINT 140
 
-#define KP_roll_pitch 0.4
-#define KI_roll_pitch 0.05
+#define KP_roll_pitch 0.5
+#define KI_roll_pitch 0.01
 #define KD_roll_pitch 100.00
 
 #define KP_yaw 3.00
 #define KI_yaw 0.05
-#define KD_yaw 800.00
+#define KD_yaw 1200.00
 
 #define YAW_INTEGRAL_LIMIT 10.00
-#define ROLL_PITCH_INTEGRAL_LIMIT 0.00
+#define ROLL_PITCH_INTEGRAL_LIMIT 0.3
 
 #define MAX_ROLL_PITCH_CONTROL_GAIN 30.00
 #define MAX_YAW_CONTROL_GAIN 25.00
