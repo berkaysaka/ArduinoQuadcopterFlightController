c18b771ded42ba72bfc023f0def4084fc5862a36
Berkay Saka <berkaysaka@yahoo.com>
2020-02-10 19:43:52 +0000

    limit integral

--- quadcopter/pid.ino
+++ quadcopter/pid.ino
@@ -1,9 +1,12 @@
 double getControlSignal(double error, double kp, double ki, double kd, double& pid_i, double& last_error, double integral_limit) {
-  pid_i += error * delta_time;
   double pid_p = error;
   double pid_d = (error - last_error) / delta_time;
+
+  pid_i += error * delta_time;
+  double integral_signal = constrain(ki*pid_i, -integral_limit, integral_limit);
+  pid_i = integral_signal / ki; 
   
-  double control_signal = (kp*pid_p) + constrain(ki*pid_i, -integral_limit, integral_limit) + (kd*pid_d);
+  double control_signal = (kp*pid_p) + integral_signal + (kd*pid_d);
   last_error = error;
   return control_signal;
 }
