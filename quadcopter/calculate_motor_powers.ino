8c2d9512797c24442cc592637d7fe962c384744c
Berkay Saka <berkaysaka@yahoo.com>
2020-05-03 00:53:00 +0100

    do not calculate pid on receiver failure as well

--- quadcopter/calculate_motor_powers.ino
+++ quadcopter/calculate_motor_powers.ino
@@ -5,7 +5,7 @@ double roll_pid_i, roll_last_error, pitch_pid_i, pitch_last_error, yaw_pid_i, ya
 bool fresh_imu_data_available = false;
 
 void calculateMotorPowers() {
-  if (imu_failure == true){
+  if (imu_failure == true || receiver_failure == true){
     last_time = millis();
     return;
   }
