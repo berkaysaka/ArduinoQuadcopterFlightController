86d1ab9b604da0a746f4cd36a5d8162d109092e8
Berkay Saka <berkaysaka@yahoo.com>
2020-05-04 00:23:11 +0100

    change asked angle method name

--- quadcopter/desired_values.ino
+++ quadcopter/desired_values.ino
@@ -1,11 +1,11 @@
 void calculateDesiredOrientation() {
   throttle = map(throttleRaw, MIN_RAW_RECEIVER_VALUE, MAX_RAW_RECEIVER_VALUE, MIN_THROTTLE, THROTTLE_LIMIT_POINT);
-  desired_pitch_angle = calculateDesiredAngle(pitchRaw, ANGLE_DEGREE_LIMIT_PITCH_ROLL);
-  desired_roll_angle = calculateDesiredAngle(rollRaw, ANGLE_DEGREE_LIMIT_PITCH_ROLL);
-  desired_yaw_angle = calculateDesiredYawAngle(desired_yaw_angle, calculateDesiredAngle(yawRaw, ANGLE_DEGREE_LIMIT_YAW));
+  desired_pitch_angle = calculateAskedAngle(pitchRaw, ANGLE_DEGREE_LIMIT_PITCH_ROLL);
+  desired_roll_angle = calculateAskedAngle(rollRaw, ANGLE_DEGREE_LIMIT_PITCH_ROLL);
+  desired_yaw_angle = calculateDesiredYawAngle(desired_yaw_angle, calculateAskedAngle(yawRaw, ANGLE_DEGREE_LIMIT_YAW));
 }
 
-double calculateDesiredAngle(int rawReceiverValue, int angleDegreeLimit) {
+double calculateAskedAngle(int rawReceiverValue, int angleDegreeLimit) {
   double center = (MIN_RAW_RECEIVER_VALUE + MAX_RAW_RECEIVER_VALUE) / 2;
   double range = MAX_RAW_RECEIVER_VALUE - MIN_RAW_RECEIVER_VALUE;
   double half_range = range / 2;
