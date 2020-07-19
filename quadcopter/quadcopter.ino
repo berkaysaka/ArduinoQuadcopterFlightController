da5e2685ccc023637abc498d7b6037ac2e65abcb
Berkay Saka <berkaysaka@yahoo.com>
2020-07-19 22:46:42 +0100

    refactor

--- quadcopter/quadcopter.ino
+++ quadcopter/quadcopter.ino
@@ -1,25 +1,31 @@
 // average flight time is 13 minutes with 10x5 props, 3000 mAh 3S lipo battery
-//-----------EXECUTION MODES---------------
+//----------- ADDITONAL MODES ---------------
 #define TELEMETRY_ENABLED false
 #define REMOTE_PID_CONFIG_ENABLED false
 
-//----------- HARDWARE CONFIGURATION-----------
-#define THROTTLE_START_POINT 15
-#define THROTTLE_LIMIT_POINT 150
-
-#define MIN_RAW_RECEIVER_VALUE 300
-#define MAX_RAW_RECEIVER_VALUE 1700
-
+//----------- PINS -----------
 #define FRONT_LEFT_MOTOR_PIN 4
 #define FRONT_RIGHT_MOTOR_PIN 6
 #define REAR_LEFT_MOTOR_PIN 5
 #define REAR_RIGHT_MOTOR_PIN 7
+#define LED_PIN 13
+#define BUZZER_PIN 12
 
+//----------- RECEIVER -----------
+#define MIN_RAW_RECEIVER_VALUE 300
+#define MAX_RAW_RECEIVER_VALUE 1700
+
+//----------- ESC's -----------
 #define MIN_MOTOR_PULSE_WIDTH 1000
 #define MAX_MOTOR_PULSE_WIDTH 2000
 
-#define LED_PIN 13
-#define BUZZER_PIN 12
+//----------- LIMITS (0-180)-----------
+#define THROTTLE_START_POINT 15
+#define THROTTLE_LIMIT_POINT 150
+double ANGLE_DEGREE_LIMIT_PITCH_ROLL = 20.00;
+double ANGLE_DEGREE_LIMIT_YAW = 5.00;
+double MAX_YAW_CONTROL_GAIN = 20.00;
+double MAX_ROLL_PITCH_CONTROL_GAIN = 30.00;
 
 //----------- PID CONFIGURATION-----------
 double KP_roll_pitch = 0.25;
@@ -31,10 +37,4 @@ double KI_yaw = 0.10;
 double KD_yaw = 0.00;
 
 double YAW_INTEGRAL_LIMIT = 10.00;
-double MAX_YAW_CONTROL_GAIN = 20.00;
-
 double ROLL_PITCH_INTEGRAL_LIMIT = 2.00;
-double MAX_ROLL_PITCH_CONTROL_GAIN = 30.00;
-
-double ANGLE_DEGREE_LIMIT_PITCH_ROLL = 20.00;
-double ANGLE_DEGREE_LIMIT_YAW = 5.00;
